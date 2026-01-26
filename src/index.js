5a0cfebf7616de7e165f044464436fe909ed9b82
use9jaehyeok <jaehyeok.lee@use9.kr>
2026-01-26 19:06:25 +0900

    feat: todo-item 삭제 기능

--- src/index.js
+++ src/index.js
@@ -1,19 +1,39 @@
 const $input = document.querySelector('.todo-input');
 const $todoArea = document.querySelector('.todo-area');
+const $submitBtn = document.querySelector('.submit-btn');
+$submitBtn.addEventListener('click', handleSubmit);
+
+let todoId = 0;
+
+
+
+function handleSubmit() {
+    $todoArea.appendChild(createTodoItem($input.value));
+    $input.value = '';
+}
+
+function handleDelete() {
+
+}
 
 function createTodoItem(value) {
     const $todoItem = document.createElement('li');
     $todoItem.classList.add('todo-item');
+    $todoItem.id = `todo-${todoId++}`;
     $todoItem.innerHTML = `
         <input type="checkbox">
         <span>${value}</span>
         <button type="button" class="delete-btn">❌</button>
     `;
+
+    const $deleteBtn = $todoItem.querySelector('.delete-btn');
+    $deleteBtn.addEventListener('click', () => {
+        $todoItem.remove();
+    });
+
     return $todoItem;
 }
 
-const $submitBtn = document.querySelector('.submit-btn');
-$submitBtn.addEventListener('click', () => {
-    $todoArea.appendChild(createTodoItem($input.value));
-    $input.value = '';
-});
+function deleteTodoItem(e) {
+    console.log(e);
+}   
